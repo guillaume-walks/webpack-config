5016190a04313cfb645edd366540ab4c10472672
guillaume <guillaume@takewalks.com>
2019-02-20 21:23:59 +0000

    extract scss into own css

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,57 @@
+'use strict'
+const { VueLoaderPlugin } = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: [
+    './src/app.js'
+  ],
+  devServer: {
+    hot: true,
+    watchOptions: {
+      poll: true
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          //'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    /**
+     * {HtmlWebpackPlugin}
+     * @inject
+     * true || 'head' || 'body' || false 
+     * Inject all assets into the given template or templateContent. 
+     * When passing true or 'body',
+     * all javascript resources will be placed at the bottom of the body element. 
+     * 'head' will place the scripts in the head element
+     */
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true 
+    }),
+    new MiniCssExtractPlugin({
+      // extract css into its own .css file
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
